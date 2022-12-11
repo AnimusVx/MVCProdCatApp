533ca9a124beab5873aa74f5be0ea4252bd6d53a
slarlardeveloper <slarlardeveloper@outlook.com>
2022-12-11 10:53:39 +0300

    Add project files.

---
+++ MVCProdCatApp/Controllers/ProductController.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCProdCatApp.Data;
+using MVCProdCatApp.Models;
+
+namespace MVCProdCatApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public ProductController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objList = _db.Product;
+
+            foreach (var obj in objList)
+            {
+                obj.Category = _db.Category.FirstOrDefault(u => u.Id == obj.CategoryId);
+            };
+
+            return View(objList);
+        }
+
+
+        //GET - UPSERT
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new Product();
+            if (id == null)
+            {
+                //this is for create
+                return View(product);
+            }
+            else
+            {
+                product = _db.Product.Find(id);
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                return View(product);
+            }
+        }
+
+
+        //POST - UPSERT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
+
+        ////GET - CREATE
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+
+        ////POST - CREATE
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Create(Category obj)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _db.Category.Add(obj);
+        //        _db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(obj);
+
+        //}
+
+
+        ////GET - EDIT
+        //public IActionResult Edit(int? id)
+        //{
+        //    if (id == null || id == 0)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var obj = _db.Category.Find(id);
+        //    if (obj == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(obj);
+        //}
+
+        ////POST - EDIT
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Edit(Category obj)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _db.Category.Update(obj);
+        //        _db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(obj);
+
+        //}
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+
+    }
+}
---
+++ MVCProdCatApp/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCProdCatApp.Models;
+
+namespace MVCProdCatApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<ApplicationType> ApplicationType { get; set; }
+        public DbSet<Product> Product { get; set; }
+    }
+}
